dafb2043169117368ad74b4efc4140ab8ea2bb4d
Kratos <desmond8500@gmail.com>
2020-09-21 11:52:10 +0000

    update

--- resources/views/0 CarryProject/layout.blade.php
+++ resources/views/0 CarryProject/layout.blade.php
@@ -5,23 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('css/vuepress.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/code.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset("library/highlight/styles/atom-one-dark.css")}}"> --}}
     <title>Carry Project</title>
 </head>
 <body>
-
     <x-carry.layout.navbar />
 
     <div class="container">
         @yield('content')
     </div>
 
-
-
     <script src="{{ asset('js/app.js')}}"></script>
-    {{-- <script src="{{ asset("library/highlight/highlight.pack.js") }}"></script> --}}
-    {{-- <script>hljs.initHighlightingOnLoad();</script> --}}
 </body>
 </html>
